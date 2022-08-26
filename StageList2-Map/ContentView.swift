deac82ee8ddf24a3bcf91b3dcae8172857133160
James Cosgrove <jamescosgrove2000@live.co.uk>
2022-08-26 14:39:42 +0100

    Initial Commit

---
+++ StageList2-Map/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StageList2-Map
+//
+//  Created by James Cosgrove on 26/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StageList2-Map/StageList2_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StageList2_MapApp.swift
+//  StageList2-Map
+//
+//  Created by James Cosgrove on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct StageList2_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
